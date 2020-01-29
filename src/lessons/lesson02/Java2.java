2140310b63472598f5912c9b01bf2e4eaae2cd54
nadzeyakuzm <nadzeyakuzm@gmail.com>
2020-01-29 20:52:34 +0300

    Latest code

--- src/lessons/lesson02/Java2.java
+++ src/lessons/lesson02/Java2.java
@@ -3,7 +3,13 @@ package lessons.lesson02;
 public class Java2 {
     public static void main(String[] args) {
         int a = 1;
-        if (a >= 1);
+        if (a >= 5) {
+            System.out.println("a is greater than 5");
+            a++;
+        } else {
+            System.out.println("a is less than 5");
+        }
+
 
     }
 }
