0e44189393ca18ac1d93b1964ff63bdf9f534a72
nadzeyakuzm <nadzeyakuzm@gmail.com>
2020-01-29 21:28:23 +0300

    Latest code

--- src/lessons/lesson02/Java2.java
+++ src/lessons/lesson02/Java2.java
@@ -10,6 +10,16 @@ public class Java2 {
             System.out.println("a is less than 5");
         }
 
+        System.out.println();
+        int q = 5;
+        int v = 80;
+        float e = 3.5f;
+        System.out.println(q + v + e + "str");
+        int i = 0;{
+        for (int i = 0; i <= 3; i++) ;
+        System.out.println(i);
+    }
+        System.out.println("The End");
 
     }
 }
