f6cdfb823e1ea6a26989e221bd1f2bd457a0f64a
nadzeyakuzm <nadzeyakuzm@gmail.com>
2020-01-29 20:36:31 +0300

    Latest code

--- src/lessons/lesson02/Java.java
+++ src/lessons/lesson02/Java.java
@@ -1,4 +1,24 @@
 package lessons.lesson02;
 
 public class Java {
+    public static void main(String[] args) {
+       byte b1=1;
+       byte b2;
+       b2=3;
+       byte b3 = (byte) (b1+b2);
+       long b4=20000000000L;
+       float b5=2.1F;
+        System.out.println();
+        char c1= 'A';
+        char c2=99;
+        System.out.println((int)c1);
+        System.out.println(c2);
+        System.out.println((char)('A' + 2));
+        int a = 1;
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println();
+        System.out.println();
+    }
 }
